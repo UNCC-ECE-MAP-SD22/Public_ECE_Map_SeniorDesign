0a341d93606b5fa0e0a3903a0d45633809323d70
Leeinfy <leeinfi1998@hotmail.com>
2021-10-07 20:26:38 -0400

    OCT 7 2021

--- overpass.py
+++ overpass.py
@@ -1,19 +1,20 @@
-from numpy import number
+from numpy import matrix, number
 import overpy
 import pandas as pd
 
 api = overpy.Overpass()
-result = api.query("way(40107019); out;")
+result = api.query("way(40107017); out;")
 way = result.ways[0]
 node = way.get_nodes(resolve_missing=True)
 
-
-#node = result.nodes[0]
-#print(node.id)
-#print(node.lat)
-#print(node.lon)
-
-df = pd.DataFrame(columns=[ 'ID', 'Longtitude', 'Latitude'])
+df = pd.DataFrame(columns=[ 'ID', 'Longtitude', 'Latitude','Connection1', 'Connection2'])
 for i in range (len(node)):
     df = df.append({'ID': node[i].id, 'Longtitude' : node[i].lon, 'Latitude' : node[i].lat}, ignore_index=True)
-print(df)
+    if i == 0:
+        df.at[i, 'Connection1'] = node[i+1].id
+    elif i == ((len(node))-1):
+        df.at[i, 'Connection1'] = node[i-1].id
+    else :
+        df.at[i, 'Connection1'] = node[i-1].id
+        df.at[i, 'Connection2'] = node[i+1].id
+df.to_csv('export_dataframe.csv', index = False, header = True)
