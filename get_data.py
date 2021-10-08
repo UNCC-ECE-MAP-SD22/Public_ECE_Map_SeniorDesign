0e4ff1feca8d6b0d1f25c8f87d252b52d79eace2
Leeinfy <leeinfi1998@hotmail.com>
2021-10-08 01:53:43 -0400

    OCT 8 2021

---
+++ get_data.py
@@ -0,0 +1,30 @@
+import overpy
+import pandas as pd
+import time
+
+OverpassAPI = overpy.Overpass()
+
+class OverPass_API_Search:
+    def QueryGetWay(str):
+        result = OverpassAPI.query(str)
+        way = result.ways[0]
+        node = way.get_nodes(resolve_missing = True)
+        return node
+
+if __name__ == "__main__":
+    #time1 = time.time()
+    df = pd.DataFrame(columns=[ 'ID', 'Longtitude', 'Latitude'])
+    node = OverPass_API_Search.QueryGetWay("way(40107017); out;")
+    #time2 = time.time()
+    for i in range (len(node)):
+        df = df.append({'ID': node[i].id, 'Longtitude' : node[i].lon, 'Latitude' : node[i].lat}, ignore_index=True)
+    #time3 = time.time()
+    i = int(df[df['ID'] == 482708531].index.values)
+    #for i in range (len(node)):
+        #print(df[df['ID'] == node[i].id])
+    #print(type(df.iloc[0]['ID']))
+    #time4 = time.time()
+    #print(time2-time1)
+    #print(time3-time2)
+    #print(time4-time3)
+    
