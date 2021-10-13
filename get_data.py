18e709d0f0780b8f1fa4c3bf764354c7e1ec4cdc
Leeinfy <leeinfi1998@hotmail.com>
2021-10-12 22:04:07 -0400

    OCT 12 2021

--- get_data.py
+++ get_data.py
@@ -1,42 +1,40 @@
 #Auther: Yinfei Li
-#Co-Worker:
 #Date: OCT 8, 2021
 
 import overpy
 import pandas as pd
-import time
 
 OverpassAPI = overpy.Overpass()
+df = pd.DataFrame(columns=[ 'ID', 'Longtitude', 'Latitude'])
 
 #Class for using OSM Overpass API
 class OverPass_API_Search:
-    #/---------------------------------------------------------
-    # Search for a way (ROAD) in OSM
+    #----------------------------------------------------------
+    # Search for a way by ID in OSM
     # Input parameter is str variable in Query search format
     # Query example "way(40107017); out;" 40107017 is way id
-    #/----------------------------------------------------------
-    def QueryGetWay(str):               
+    #----------------------------------------------------------
+    def QueryGetWaybyID(str):               
         result = OverpassAPI.query(str)
         way = result.ways[0]
         node = way.get_nodes(resolve_missing = True)
         return node
+    #---------------------------------------------------------------
+    # Search for all node way or relationship within a box in OSM
+    # Query example "[out:xml]; node(50.745, 7.17, 50.75, 7.18); out;"
+    # in the box parameter "node(south, west, north, est)"
+    # node could change to way, relationship, nw, nwr
+    #---------------------------------------------------------------
+    def QueryGetInfobyBox(str):
+        result = OverpassAPI.query(str)
+        node = result.nodes
+        way = result.ways
+        return node, way
+
 
 if __name__ == "__main__":
-    #time1 = time.time()
-    df = pd.DataFrame(columns=[ 'ID', 'Longtitude', 'Latitude'])
-    node = OverPass_API_Search.QueryGetWay("way(40107017); out;")
-    node2 = OverPass_API_Search.QueryGetWay("way(); out;")
-    #time2 = time.time()
+    a = "[out:xml];node(35.30716, -80.74561, 35.31319, -80.73666);out;"
+    node, way =  OverPass_API_Search.QueryGetInfobyBox(a)
     for i in range (len(node)):
         df = df.append({'ID': node[i].id, 'Longtitude' : node[i].lon, 'Latitude' : node[i].lat}, ignore_index=True)
-    #time3 = time.time()
-    #i = int(df[df['ID'] == 482708531].index.values)
-    #print(i)
-    #for i in range (len(node)):
-        #print(df[df['ID'] == node[i].id])
-    #print(type(df.iloc[0]['ID']))
-    #time4 = time.time()
-    #print(time2-time1)
-    #print(time3-time2)
-    #print(time4-time3)
-    
+    df.to_csv('export_dataframe.csv', index = False, header = True)
