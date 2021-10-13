00ff72d34aa8a1cc0af08e7b52bbdc46a627fb74
leeinfy <66580144+leeinfy@users.noreply.github.com>
2021-10-12 22:07:40 -0400

    Delete OpenStreetMap.py

--- OpenStreetMap.py
+++
@@ -1,9 +0,0 @@
-import osmnx as ox
-import matplotlib.pyplot as plt
-
-place_name = "The University of North Carolina at Charlotte"
-
-graph = ox.graph_from_place(place_name)
-
-type(graph)
-fig, ax = ox.plot_graph(graph)
